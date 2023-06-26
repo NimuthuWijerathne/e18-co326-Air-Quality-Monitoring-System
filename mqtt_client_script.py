af8d46b9f0674592db36398156e8b3ff8f2ceae6
HirushaUthsara <e18368@eng.pdn.ac.lk>
2023-06-26 10:33:10 +0530

    connect to mongodb

--- mqtt_client_script.py
+++ mqtt_client_script.py
@@ -1,5 +1,5 @@
-import tkinter as tk
 import paho.mqtt.client as mqtt
+from pymongo import MongoClient
 
 mqtt_broker = "test.mosquitto.org"
 mqtt_port = 1883
@@ -10,26 +10,33 @@ topic_LED = "326/grp19/Actuators/LED/"
 topic_buzzer = "326/grp19/Actuators/Buzzer/"
 topic = "326/grp19/"
 
+# MongoDB connection details
+mongo_host = 'localhost'
+mongo_port = 27017
+mongo_database = 'sensor_data'
+mongo_collection = 'sensors'
+
 def on_connect(client, userdata, flags, rc):
     print("Connected to MQTT broker")
     client.subscribe(topic_temp)
     client.subscribe(topic_humidity)
     client.subscribe(topic_airquality)
-    client.subscribe(topic_LED)
-    client.subscribe(topic_buzzer)
 
 def on_message(client, userdata, msg):
     print("Received message: " + msg.topic + " " + str(msg.payload))
-    # Update the message in the UI
-    received_message.set("Received message: " + msg.topic + " " + str(msg.payload))
+    # Save sensor data to MongoDB
+    data = {
+        'topic': msg.topic,
+        'payload': str(msg.payload)
+    }
+    save_to_mongodb(data)
 
-def send_command():
-    # Get the command from the input field
-    command = command_entry.get()
-    # Publish the command to the respective topic
-    client.publish(topic_LED, command)  # Modify the topic as per your requirement
-    # Clear the input field
-    command_entry.delete(0, tk.END)
+def save_to_mongodb(data):
+    client = MongoClient(mongo_host, mongo_port)
+    db = client[mongo_database]
+    collection = db[mongo_collection]
+    collection.insert_one(data)
+    client.close()
 
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -37,29 +44,12 @@ client.on_message = on_message
 
 client.connect(mqtt_broker, mqtt_port, 60)
 
-# Create the Tkinter UI
-root = tk.Tk()
-root.title("MQTT Client")
-root.geometry("400x300")
-
-# Message label to display incoming messages
-received_message = tk.StringVar()
-message_label = tk.Label(root, textvariable=received_message)
-message_label.pack()
-
-# Command entry field
-command_entry = tk.Entry(root, width=30)
-command_entry.pack()
-
-# Send button to send commands
-send_button = tk.Button(root, text="Send", command=send_command)
-send_button.pack()
-
 # Start the MQTT client loop in a separate thread
 client.loop_start()
 
-# Run the Tkinter event loop
-root.mainloop()
+# Run indefinitely
+while True:
+    pass
 
-# Stop the MQTT client loop after closing the UI window
+# Stop the MQTT client loop (will never reach this point)
 client.loop_stop()
