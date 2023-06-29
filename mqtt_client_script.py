3347dbc61f86fe0a0f21e249923fc3ecfc163824
HirushaUthsara <e18368@eng.pdn.ac.lk>
2023-06-29 23:34:15 +0530

    database script added

--- mqtt_client_script.py
+++ mqtt_client_script.py
@@ -1,47 +1,101 @@
 import paho.mqtt.client as mqtt
-from pymongo import MongoClient
+import mysql.connector
 
+# MQTT topics
+topic_temp = "UoP/CO/326/E18/19/DHT11/temperature"
+topic_humidity = "UoP/CO/326/E18/19/DHT11/humidity"
+topic_airquality = "UoP/CO/326/E18/19/MQ135/AirQuality"
+topic_LED = "UoP/CO/326/E18/19/LED"
+topic_buzzer = "UoP/CO/326/E18/19/Buzzer"
+
+
+# MQTT broker details
 mqtt_broker = "test.mosquitto.org"
 mqtt_port = 1883
-topic_temp = "326/grp19/sensors/DHT11/temp"
-topic_humidity = "326/grp19/sensors/DHT11/humi"
-topic_airquality = "326/grp19/sensors/MQ135/"
-topic_LED = "326/grp19/Actuators/LED/"
-topic_buzzer = "326/grp19/Actuators/Buzzer/"
-topic = "326/grp19/"
 
-# MongoDB connection details
-mongo_host = 'localhost'
-mongo_port = 27017
-mongo_database = 'sensor_data'
-mongo_collection = 'sensors'
+# MySQL database details
+mysql_host = "localhost"
+mysql_user = "root"
+mysql_password = ""
+mysql_database = "326_project"
+
+
+# MQTT topics
+topics = {
+    "temperature": "UoP/CO/326/E18/19/DHT11/temperature",
+    "humidity": "UoP/CO/326/E18/19/DHT11/humidity",
+    "air_quality": "UoP/CO/326/E18/19/MQ135/AirQuality",
+    "LED": "UoP/CO/326/E18/19/LED",
+    "buzzer": "UoP/CO/326/E18/19/Buzzer"
+}
+
+sensor_data = {
+    "temperature": None,
+    "humidity": None,
+    "air_quality": None
+}
+
 
 def on_connect(client, userdata, flags, rc):
     print("Connected to MQTT broker")
-    client.subscribe(topic_temp)
-    client.subscribe(topic_humidity)
-    client.subscribe(topic_airquality)
+    # Subscribe to all topics
+    for topic in topics.values():
+        client.subscribe(topic)
+
 
 def on_message(client, userdata, msg):
     print("Received message: " + msg.topic + " " + str(msg.payload))
-    # Save sensor data to MongoDB
-    data = {
-        'topic': msg.topic,
-        'payload': str(msg.payload)
-    }
-    save_to_mongodb(data)
+    # Store sensor data
+    store_sensor_data(msg.topic, msg.payload.decode())
 
-def save_to_mongodb(data):
-    client = MongoClient(mongo_host, mongo_port)
-    db = client[mongo_database]
-    collection = db[mongo_collection]
-    collection.insert_one(data)
-    client.close()
 
+def store_sensor_data(topic, payload):
+    # Update the corresponding sensor value
+    for sensor, topic_name in topics.items():
+        if topic == topic_name:
+            sensor_data[sensor] = float(payload)
+
+    # If all sensor data is available, insert into the database
+    if all(value is not None for value in sensor_data.values()):
+        insert_data()
+
+
+def insert_data():
+    try:
+        # Connect to MySQL database
+        conn = mysql.connector.connect(
+            host=mysql_host,
+            user=mysql_user,
+            password=mysql_password,
+            database=mysql_database
+        )
+        cursor = conn.cursor()
+
+        # Create SQL query to insert data into the table
+        sql = "INSERT INTO sensor_data (temperature, humidity, air_quality) VALUES (%s, %s, %s)"
+        values = tuple(sensor_data.values())
+
+        # Execute the query
+        cursor.execute(sql, values)
+        conn.commit()
+
+        # Reset sensor data
+        for sensor in sensor_data:
+            sensor_data[sensor] = None
+
+        # Close the database connection
+        cursor.close()
+        conn.close()
+    except mysql.connector.Error as error:
+        print("Error while connecting to MySQL:", error)
+
+
+# MQTT client setup
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
+# Connect to MQTT broker
 client.connect(mqtt_broker, mqtt_port, 60)
 
 # Start the MQTT client loop in a separate thread
